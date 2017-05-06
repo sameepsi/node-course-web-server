f6afade0c43c9b044a8992840d51e032397d8e24
Sameep Singhania <sameepsi@gmail.com>
2017-05-06 18:23:06 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const fs= require('fs');
+
+var app = express();
+var hbs = require('hbs');
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+
+app.use((req, res, next)=>{
+  var date=new Date().toString();
+  var log = `${date}: ${req.method}: ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+"\n", (err)=>{
+    if(err){
+      console.log("Unable to write log to the file server.log!!")
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next)=>{
+//   res.render('maintenance.hbs', {
+//       pageTitle:'Maintenance',
+//       header:'Maintenance is underway',
+//       welcomeMessage:'Sorry currently our website is under maintenance. We will be up soon !!'
+//   });
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+})
+
+ app.get('/',(req, res)=>{
+  //  res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    welcomeMessage : 'I welcome you to this page'
+  })
+ });
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+  pageTitle:'About Page'
+});
+  // res.send('Inside about');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage:'Bad request!!'
+  });
+});
+ app.listen(3000, ()=>{
+   console.log('Server is up on port 3000');
+ });
